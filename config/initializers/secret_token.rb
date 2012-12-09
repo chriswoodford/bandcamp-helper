c1e5966ad9df360e4a08f55337a8bd068e184386
Chris Woodford <chris.woodford@gmail.com>
2012-12-09 17:50:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BandcampHelper::Application.config.secret_token = 'b01a6fe58e64440b069ff9cb1a2f13fcb45c32be8543eeac1c4d20939d2fc9574816bd02405fb4d86b4e85b148930c219529175ed0293c088c375ddee5aecf9d'
